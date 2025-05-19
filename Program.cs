55fe0a4e90167b157e2be18eef3652e17acc4d6f
MohammadHoseinRoohi <mohammadhoseinroohi4@gmail.com>
2025-05-20 00:14:09 +0330

    First commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Hello!");
+var p1 = new Pirahan();
+var p2 = new Pirahan();
+
+
+
+
+
+
+public class Pirahan()
+{
+    public Parche? Parche { get; set; }
+    public Dokme? Dokme { get; set; }
+
+}
+public class Parche()
+{
+
+}
+public class Dokme()
+{
+
+}
